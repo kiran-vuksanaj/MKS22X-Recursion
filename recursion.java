b40b4c38279ad1b6d654558b41fa171ef8f77344
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-31 22:20:13 -0500

    base code from website with meaningless return statements

---
+++ recursion.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class recursion{
+  /*You may write additional private methods */
+
+  /*Recursively find the sqrt using Newton's approximation
+   *tolerance is the allowed percent error the squared answer is away from n.
+   *precondition: n is non-negative
+
+  */
+  public static double sqrt(double n, double tolerance){
+    return 0.0;
+  }
+
+  /*Recursively find the n'th fibbonaci number in linear time
+   *fib(0) = 1; fib(1) = 1; fib(5) = 5
+   *precondition: n is non-negative
+   */
+  public static int fib(int n){
+    return 0;
+  }
+
+  /*As Per classwork*/
+  public static ArrayList<Integer> makeAllSums(){
+    return null;
+  }
+}
